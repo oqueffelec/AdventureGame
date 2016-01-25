b4456b16408bb91c3612a40c0e1f7179c1fb95dd
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-01-25 16:32:58 +0100

    maj

--- src/fr/insarouen/asi/prog/asiaventure/Main.java
+++ src/fr/insarouen/asi/prog/asiaventure/Main.java
@@ -1,8 +1,11 @@
 package fr.insarouen.asi.prog.asiaventure;
 
+import fr.insarouen.asi.prog.asiaventure.*;
+
 public class Main{
 
 	public static void main(String[] args) {
 
+		Monde m=new Monde("hi");
 	}
 }
--- src/fr/insarouen/asi/prog/asiaventure/Monde.java
+++ src/fr/insarouen/asi/prog/asiaventure/Monde.java
@@ -1,10 +1,30 @@
 package fr.insarouen.asi.prog.asiaventure;
 
+import fr.insarouen.asi.prog.asiaventure.Entite;
+
 public class Monde extends java.lang.Object implements java.io.Serializable{
 
 	private String nomDuMonde;
+	private Entite[] entites=new Entite[0];
 
-	public void Monde(String nom){
-		this.nomDuMonde=nom;
+	public void Monde(String nomDuMonde){
+		this.nomDuMonde=nomDuMonde;
+	}
+
+	public void ajouter(Entite entite){
+		Entite[] tmp=new Entite[this.entites.length+1];
+		for(int i=0;i<this.entites.length;i++){
+			tmp[i]=this.entites[i];
+		}
+		tmp[tmp.length-1]=entite;
+		this.entites=tmp;
+	}
+
+	public Entite getEntite(String nomEntite){
+		for(int i=0;i<this.entites.length;i++){
+			if(this.entites[i].getNom().equals(nomEntite))
+				return this.entites[i];
+		}
+		return null;
 	}
 }
