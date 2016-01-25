b01e325ade4222b239d8a5594e5980007f018d8b
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-01-25 17:28:03 +0100

    maj

---
+++ src/fr/insarouen/asi/prog/asiaventure/elements/Entite.java
@@ -0,0 +1,38 @@
+package fr.insarouen.asi.prog.asiaventure.elements;
+
+import fr.insarouen.asi.prog.asiaventure.Monde;
+
+public class Entite extends java.lang.Object implements java.io.Serializable {
+  private java.lang.String nomEntite;
+  private Monde monde;
+
+  //Constructeur//
+
+  public Entite(java.lang.String nom, Monde monde) {
+    this.nomEntite=nom;
+    this.monde=monde;
+  }
+  // Methodes//
+
+  public java.lang.String getNom() {
+    return(this.nomEntite);
+  }
+
+  public Monde getMonde() {
+    return(this.monde);
+  }
+    public java.lang.String toString() {
+    return(String.format("Nom de l'entité : %s",this.nomEntite));
+  }
+
+  public boolean equals(java.lang.Object o) {
+    if (o==this)
+      return true;
+    if (o.getClass()==getClass()) {
+      Entite entite=(Entite)o;
+      return entite.getNom().equals(getNom()) && entite.getMonde()==this.getMonde();
+    }
+    return false;
+  }
+
+}
