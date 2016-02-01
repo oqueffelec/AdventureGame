123891cb8bc95ad1e28476d2bdfcf9d940d55b4d
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-02-01 16:49:42 +0100

    Piece

--- src/fr/insarouen/asi/prog/asiaventure/Main.java
+++ src/fr/insarouen/asi/prog/asiaventure/Main.java
@@ -14,18 +14,24 @@ public class Main{
 		//test entité
 		Entite e=new Entite("Archer",m){};
 		Entite e2=new Entite("Berserker",m){};
+		System.out.println(e.equals(e)+" "+e.equals(e2));
 
 		//test Monde
 		m.ajouter(e);m.ajouter(e2);
 		System.out.println(m);
 
 		//test objet
-		Objet o=new Objet("marteau",m){
+		Objet o1=new Objet("marteau",m){
 				public boolean estDeplacable(){
 					return true;
 				}
 		};
-		System.out.println(o);
+		Objet o2=new Objet("carquois",m){
+				public boolean estDeplacable(){
+					return true;
+				}
+		};
+		System.out.println(o2);
 
 		//test element structurel
 		Entite elem=new ElementStructurel("hache",m){};
@@ -33,5 +39,14 @@ public class Main{
 
 		//test PiedDeBiche
 		Objet p=new PiedDeBiche("p1",m){};
+		System.out.println(p);
+
+		//test Piece
+		Piece piece=new Piece("Salle de torture",m);
+		piece.deposer(o1);
+		piece.deposer(o2);
+		System.out.println(piece);
+		System.out.println(piece.contientObjet(o1));
+		System.out.println(piece.contientObjet("marteaus"));
 	}
 }
