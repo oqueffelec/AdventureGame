d261289411919d1a862c46c8eae80cc231239e92
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-02-29 16:18:04 +0100

    update makefile

--- srctest/fr/insarouen/asi/prog/asiaventure/TestMonde.java
+++ srctest/fr/insarouen/asi/prog/asiaventure/TestMonde.java
@@ -1,6 +1,7 @@
 package fr.insarouen.asi.prog.asiaventure;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.hamcrest.core.*;
 import fr.insarouen.asi.prog.asiaventure.Monde;
 
 public class TestMonde{
@@ -8,6 +9,6 @@ public class TestMonde{
   @Test
   public void testConstructeur(){
     Monde monde=new Monde("Neverwinter");
-    assertTrue(monde.getNom().equals("Neverwinter"));
+    assertThat(monde.getNom(),IsEqual.equalTo("Neverwinter"));
   }
 }
