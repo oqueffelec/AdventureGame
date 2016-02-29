a5b04046adbaf9a068e9583f65bbc44f3d661441
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-02-29 16:50:33 +0100

    update makefile

--- srctest/fr/insarouen/asi/prog/asiaventure/TestMonde.java
+++ srctest/fr/insarouen/asi/prog/asiaventure/TestMonde.java
@@ -1,14 +1,38 @@
 package fr.insarouen.asi.prog.asiaventure;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 import org.hamcrest.core.*;
 import fr.insarouen.asi.prog.asiaventure.Monde;
+import fr.insarouen.asi.prog.asiaventure.elements.Entite;
 
 public class TestMonde{
 
+  Monde monde;
+  Entite entite,entite2;
+
+  @Before
+  public void avantTest(){
+    monde=new Monde("Neverwinter");
+    try{
+      entite=new Entite("Archer",monde){};
+    }
+    catch(NomDEntiteDejaUtiliseDansLeMondeException e){
+  		System.err.println(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testgetNom(){
+    assertEquals(monde.getNom(),"Neverwinter");
+  }
+
   @Test
   public void testConstructeur(){
-    Monde monde=new Monde("Neverwinter");
-    assertThat(monde.getNom(),IsEqual.equalTo("Neverwinter"));
+    assertEquals(monde.getNom(),"Neverwinter");
+  }
+
+  @Test(expected=NomDEntiteDejaUtiliseDansLeMondeException.class,EntiteDejaDansUnAutreMondeException.class)
+  public void testajouter(){
+    monde.ajouter(new Entite("Archer",monde){});
   }
 }
