529be5ec8c7097bb5820aeca95ee982a73bb7502
queffelec <octave.queffelec@insa-rouen.fr>
2016-03-05 20:46:52 +0100

    pb avec la declaration des vivants

--- srctest/fr/insarouen/asi/prog/asiaventure/elements/vivants/TestVivant.java
+++ srctest/fr/insarouen/asi/prog/asiaventure/elements/vivants/TestVivant.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.hamcrest.core.*;
 import fr.insarouen.asi.prog.asiaventure.*;
 import fr.insarouen.asi.prog.asiaventure.elements.Entite;
-import fr.insarouen.asi.prog.asiaventure.elements.vivants.*;
+import fr.insarouen.asi.prog.asiaventure.elements.vivants.Vivant;
 import fr.insarouen.asi.prog.asiaventure.elements.structure.Piece;
 import fr.insarouen.asi.prog.asiaventure.elements.objets.Objet;
 
@@ -15,7 +15,9 @@ public class TestVivant{
   Piece piece;
   Entite entite;
   Objet o1,o2;
+
   @Before
+
   public void avantTest() throws NomDEntiteDejaUtiliseDansLeMondeException{
      monde= new Monde("monde");
      piece=new Piece("Salle de Torture",monde);
@@ -29,11 +31,29 @@ public class TestVivant{
              return true;
            }
        };
+     Vivant vivant=new Vivant("Drizzt",monde,0,0,piece,o1){};
+
     }
 
     @Test
     public void testestMort() throws NomDEntiteDejaUtiliseDansLeMondeException{
-      vivant=new Vivant("Drizzt",monde,0,0,piece,o1){};
       assertTrue(vivant.estMort());
+    }
+
+    @Test
+    public void testDeposer() {
+      vivant.deposer(o1);
+      assertFalse(vivant.possede(o1));
+    }
+
+    @Test
+    public void testPrendre(){
+      vivant.prendre(o1);
+      assertTrue(vivant.possede(o1));
+    }
+
+    @Test(expected=NomDEntiteDejaUtiliseDansLeMondeException.class)
+    public void testConstructeurVivant(){
+      Vivant vivant2=new Vivant("Drizzt",monde,0,0,piece,o1){};
     }
 }
