d7ad036c892ce9056c0891b5c21625b70cf65638
queffelec <octave.queffelec@insa-rouen.fr>
2016-03-21 16:17:48 +0100

    simulateur

---
+++ src/fr/insarouen/asi/prog/asiaventure/Simulateur.java
@@ -0,0 +1,35 @@
+package fr.insarouen.asi.prog.asiaventure;
+
+import fr.insarouen.asi.prog.asiaventure.*;
+import fr.insarouen.asi.prog.asiaventure.elements.*;
+import fr.insarouen.asi.prog.asiaventure.elements.vivants.*;
+import fr.insarouen.asi.prog.asiaventure.elements.objets.*;
+import fr.insarouen.asi.prog.asiaventure.elements.structure.*;
+import fr.insarouen.asi.prog.asiaventure.lib.ListeObjet;
+import fr.insarouen.asi.prog.asiaventure.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+
+
+public class Simulateur extends java.lang.Object {
+
+  // Constructeur
+
+  public Simulateur(ObjectInputStream ois) throws IOException,ClassNotFoundException{
+
+  }
+  public Simulateur(Reader reader) throws IOException{
+
+  }
+
+  // Methodes
+
+  public void enregistrer(ObjectOutputStream oos) throws IOException{
+    
+  }
+}
