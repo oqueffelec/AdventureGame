8ef126a5065eb6885e0eee2230e938d32c1660c6
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-05-02 15:47:39 +0200

    MAJ

---
+++ Swing/src/TextFieldEffacable.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TextFieldEffacable extends JTextField {
+
+  public TextFieldEffacable(String titre,int i){
+    super(titre,i);
+  }
+}
