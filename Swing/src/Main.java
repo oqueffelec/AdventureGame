103a2b74edae5409f114e9ab8d9c0748b29758a2
Alexandre Le Lain <alexandre.le_lain@insa-rouen.fr>
2016-05-02 16:34:29 +0200

    MAJ

--- Swing/src/Main.java
+++ Swing/src/Main.java
@@ -7,12 +7,35 @@ public class Main{
   public static void main(String[] args){
 
       //Exercice 1
+      /*
       JFrame exo1 = new JFrame("Exercice 1");
       TextFieldEffacable tf1 = new TextFieldEffacable("Entrez du texte ici",50);
       Container cp = exo1.getContentPane();
       cp.add(tf1);
       exo1.pack();
-      exo1.setVisible(true);
+      exo1.setVisible(true);*/
 
+      //Exercie 2
+      //2.1
+      JFrame exo2 = new JFrame("Exercice 2");
+      TextFieldEffacableAdapter tf2 = new TextFieldEffacableAdapter("Entrez du texte ici",50);
+      Container cp = exo2.getContentPane();
+      cp.add(tf2);
+      exo2.pack();
+      exo2.setVisible(true);
+
+      //2.2
+      /*
+      JFrame exo2 = new JFrame("Exercice 2");
+      JTextField tf2 = new JTextField("Entrez du texte ici",50);
+      tf2.addMouseListener(new MouseAdapter(){
+                               public void mouseClicked(MouseEvent e){
+                                 tf2.setText("");
+                               }
+                             });
+      Container cp = exo2.getContentPane();
+      cp.add(tf2);
+      exo2.pack();
+      exo2.setVisible(true);*/
   }
 }
