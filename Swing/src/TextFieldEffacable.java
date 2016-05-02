4fccf157c988cc0a7c0722da74c773dd1a1ac2dd
Queffelec Octave <octave.queffelec@insa-rouen.fr>
2016-05-02 15:57:06 +0200

    exo1

--- Swing/src/TextFieldEffacable.java
+++ Swing/src/TextFieldEffacable.java
@@ -2,9 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TextFieldEffacable extends JTextField {
+public class TextFieldEffacable extends JTextField implements MouseListener{
 
   public TextFieldEffacable(String titre,int i){
     super(titre,i);
+    this.addMouseListener(this);
   }
+  public void mouseClicked(MouseEvent e){
+    this.setText("");
+  }
+  public void mousePressed(MouseEvent e){}
+  public void mouseReleased(MouseEvent e){}
+  public void mouseEntered(MouseEvent e){}
+  public void mouseExited(MouseEvent e){}
+
 }
